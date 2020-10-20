dd75d102922101dab6980f1aa62328e3aa28e13c
Muhammed Okumuş <rocketpolka@gmail.com>
2020-10-20 17:54:21 +0300

    Deleted API key and secret

--- Project/Examples/request.py
+++ Project/Examples/request.py
@@ -1,16 +1,16 @@
 import requests
 
 postUrl='http://panel.vatansms.com/panel/smsgonder1Npost.php'
-musteriNo='39944'
-kullaniciAdi='905350274019'
-sifre='190wud40'
+musteriNo=''
+kullaniciAdi=''
+sifre=''
 orjinator="SMS TEST"
 
 tur='Normal' """ Normal yada Turkce """
 zaman=''
 
 mesaj='Bu bir test mesajidir GTU.'
-numara1='05350274019'
+numara1=''
 
 string = """
 <sms>
@@ -30,3 +30,4 @@ string = """
 response =  requests.post(postUrl, data={"data":string})
 
 print(response.text)
+
